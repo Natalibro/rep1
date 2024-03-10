3a5e17b869c8fceac9b0ea6ccd50ec4a528b8813
Natalibro <brodka@mail.ru>
2024-03-11 01:54:23 +0300

    tt3

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/// Задайте произвольный массив. Выведете его элементы, начиная с конца.
+// Использовать рекурсию, не использовать циклы.
+
+Console.Clear();
+int[]array = [1,2,5,0,10,34];
+int size = array.Length;
+void ArrayReverse (int[]array, int size)
+{
+    if (size==0)
+    {
+        return;
+    }
+
+Console.Write($"{array[size-1]} ");
+ArrayReverse(array, size-1);
+}
+ArrayReverse(array,size);
